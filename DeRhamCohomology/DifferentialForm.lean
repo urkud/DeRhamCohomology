9da3145ff8235bd8c708dcf7b0d476199a8d8a80
MetalCreator666 <57985847+MetalCreator666@users.noreply.github.com>
2024-11-29 20:27:26 +0100

    pullback definition + first properties

--- DeRhamCohomology/DifferentialForm.lean
+++ DeRhamCohomology/DifferentialForm.lean
@@ -5,11 +5,6 @@ import DeRhamCohomology.ContinuousAlternatingMap.FDeriv
 /-
 # Differential Forms
 
-## Main definitions
-* `DifferentialForm E F n`
-* `extDeriv`
-
-
 -/
 
 noncomputable section
@@ -17,27 +12,54 @@ noncomputable section
 open Filter ContinuousAlternatingMap
 open scoped Topology
 
-variable {E F : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
-  [NormedAddCommGroup F] [NormedSpace ℝ F] {n : ℕ}
+variable {E F G : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
+  [NormedAddCommGroup F] [NormedSpace ℝ F]
+  [NormedAddCommGroup G] [NormedSpace ℝ G] {n k : ℕ}
 
 -- TODO: change notation
 notation "Ω^" n "⟮" E ", " F "⟯" => E → E [⋀^Fin n]→L[ℝ] F
 
-instance : Zero (DifferentialForm E F n) :=
+variable {v : E}
+variable (ω τ : Ω^n⟮E, F⟯)
+variable (f : E → F)
+
+instance : Add (Ω^n⟮E, F⟯) :=
+  ⟨ (fun ω τ => fun v => ω v + τ v) ⟩
+
+@[simp]
+theorem add_apply : (ω + τ) v = ω v + τ v :=
+  rfl
+
+instance : Sub (Ω^n⟮E, F⟯) :=
+  ⟨ (fun ω τ => fun v => ω v - τ v) ⟩
+
+@[simp]
+theorem sub_apply : (ω - τ) v = ω v - τ v :=
+  rfl
+
+instance : Neg (Ω^n⟮E, F⟯) :=
+  ⟨fun ω => fun v => -ω v⟩
+
+@[simp]
+theorem neg_apply : (-ω) v = -ω v :=
+  rfl
+
+instance : SMul ℝ (Ω^n⟮E, F⟯) :=
+  ⟨fun c ω => fun v => c • ω v⟩
+
+@[simp]
+theorem smul_apply (ω : Ω^n⟮E, F⟯) (c : ℝ) (v : E): (c • ω) v = c • ω v :=
+  rfl
+
+instance : Zero (Ω^n⟮E, F⟯) :=
   ⟨ (fun _ => 0) ⟩
 
 @[simp]
-theorem zero_apply : (0 : DifferentialForm E F n) v = 0 :=
+theorem zero_apply : (0 : Ω^n⟮E, F⟯) v = 0 :=
   rfl
 
-instance inhabited : Inhabited (DifferentialForm E F n) :=
+instance inhabited : Inhabited (Ω^n⟮E, F⟯) :=
   ⟨0⟩
-
-/- Application of differential form to vector field -/
-def differentialform_apply_vectorfield {n : ℕ} (ω : DifferentialForm E F (n + 1)) (ξ : E → (Fin 1 → E)) :=
-  fun v ↦ (ω v).curryFin (ξ v 0)
-
-
 
 /-- Exterior derivative of a differential form. -/
 def ederiv (ω : Ω^n⟮E, F⟯) : Ω^n + 1⟮E, F⟯ :=
@@ -69,3 +91,27 @@ theorem ederiv_ederiv_apply (ω : Ω^n⟮E, F⟯) {x} (h : ContDiffAt ℝ 2 ω x
 
 theorem ederiv_ederiv (ω : Ω^n⟮E, F⟯) (h : ContDiff ℝ 2 ω) : ederiv (ederiv ω) = 0 :=
   funext fun x ↦ ederiv_ederiv_apply ω h.contDiffAt
+
+/- Pullback of a differential form -/
+def pullback (f : E → F) (ω : Ω^k⟮F, G⟯) : Ω^k⟮E, G⟯ :=
+  fun x ↦ (ω (f x)).compContinuousLinearMap (fderiv ℝ f x)
+
+theorem pullback_zero (f : E → F) :
+  pullback f (0 : Ω^k⟮F, G⟯) = 0 :=
+    rfl
+
+theorem pullback_add (f : E → F) (ω : Ω^k⟮F, G⟯) (τ : Ω^k⟮F, G⟯) :
+  pullback f (ω + τ) = pullback f ω + pullback f τ :=
+    rfl
+
+theorem pullback_sub (f : E → F) (ω : Ω^k⟮F, G⟯) (τ : Ω^k⟮F, G⟯) :
+  pullback f (ω - τ) = pullback f ω - pullback f τ :=
+    rfl
+
+theorem pullback_neg (f : E → F) (ω : Ω^k⟮F, G⟯) :
+  - pullback f ω = pullback f (-ω) :=
+    rfl
+
+theorem pullback_smul (f : E → F) (ω : Ω^k⟮F, G⟯) (c : ℝ) :
+  c • (pullback f ω) = pullback f (c • ω) :=
+    rfl
