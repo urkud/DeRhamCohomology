334fbe5af953e9abab79c14f8a7b3c5fb9a3bee4
MetalCreator666 <57985847+MetalCreator666@users.noreply.github.com>
2024-12-18 16:20:59 +0100

    Additions to `LinearIsometryEquiv.flipMultilinear`

--- DeRhamCohomology/Multilinear/Basic.lean
+++ DeRhamCohomology/Multilinear/Basic.lean
@@ -14,9 +14,58 @@ def LinearIsometryEquiv.flipMultilinear :
     (G →L[𝕜] ContinuousMultilinearMap 𝕜 E G') ≃ₗᵢ[𝕜]
       (ContinuousMultilinearMap 𝕜 E (G →L[𝕜] G')) where
   toFun := ContinuousLinearMap.flipMultilinear
-  invFun := sorry
-  map_add' := sorry
-  map_smul' := sorry
-  left_inv := sorry
-  right_inv := sorry
-  norm_map' := sorry
+  invFun f :=
+    LinearMap.mkContinuous
+      { toFun := fun x =>
+          MultilinearMap.mkContinuous
+            { toFun := fun m => f m x
+              map_update_add' := fun m i x y => by simp
+              map_update_smul' := fun m i c x => by simp }
+            (‖f‖*‖x‖) fun m => by
+              simp only [MultilinearMap.coe_mk]
+              rw[mul_right_comm]
+              refine le_trans ((f m).le_opNorm x) ?hg
+              exact mul_le_mul_of_nonneg_right (f.le_opNorm m) (norm_nonneg x)
+        map_add' := fun x y => by
+          ext1
+          simp only [ContinuousMultilinearMap.add_apply, MultilinearMap.coe_mkContinuous,
+            MultilinearMap.coe_mk, ContinuousLinearMap.map_add]
+        map_smul' := fun c x => by
+          ext1
+          simp [ContinuousMultilinearMap.smul_apply, MultilinearMap.coe_mkContinuous,
+            MultilinearMap.coe_mk, ContinuousLinearMap.map_smul_of_tower] }
+      ‖f‖ fun x => by
+        simp
+        -- Following should be simplified
+        -- Not sure how exactly yet (probably using `MultilinearMap.coe_mkContinuous`,
+        --  and `MultilinearMap.coe_mk`)
+        refine
+          MultilinearMap.mkContinuous_norm_le
+            { toFun := fun m ↦ (f m) x,
+              map_update_add' := fun [DecidableEq ι] m i x_1 y ↦
+                of_eq_true
+                  (Eq.trans
+                    (congrArg
+                      (fun x_2 ↦
+                        x_2 x = (f (Function.update m i x_1)) x + (f (Function.update m i y)) x)
+                      (ContinuousMultilinearMap.map_update_add f m i x_1 y))
+                    (eq_self ((f (Function.update m i x_1)) x + (f (Function.update m i y)) x))),
+              map_update_smul' := fun [DecidableEq ι] m i c x_1 ↦
+                of_eq_true
+                  (Eq.trans
+                    (congrArg (fun x_2 ↦ x_2 x = c • (f (Function.update m i x_1)) x)
+                      (ContinuousMultilinearMap.map_update_smul f m i c x_1))
+                    (eq_self (c • (f (Function.update m i x_1)) x))) }
+            ?hC fun m ↦
+            id
+              (Eq.mpr
+                (id (congrArg (fun _a ↦ ‖(f m) x‖ ≤ _a) (mul_right_comm ‖f‖ ‖x‖ (∏ i : ι, ‖m i‖))))
+                (le_trans (ContinuousLinearMap.le_opNorm (f m) x)
+                  (mul_le_mul_of_nonneg_right (ContinuousMultilinearMap.le_opNorm f m)
+                    (norm_nonneg x))))
+        exact Right.mul_nonneg (norm_nonneg f) (norm_nonneg x)
+  map_add' _ _ := rfl
+  map_smul' _ _ := rfl
+  left_inv := congrFun rfl
+  right_inv := congrFun rfl
+  norm_map' := fun x => by sorry -- TODO
